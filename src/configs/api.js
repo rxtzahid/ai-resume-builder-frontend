3d0de7810f4ccbf8f529f1c7518c0f4e8d080af3
RXT ZAHID HOSSAIN <you@example.com>
2025-11-15 00:40:25 +0600

    added files

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 const api= axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL
+    baseURL: import.meta.env.VITE_BASE_URL || import.meta.env.VITE_PRODUCTION_BASE_URL
 })
 export default api
